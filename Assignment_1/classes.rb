e81aad2703aeee71a1bef099ddc49b3e22a0e4d7
Carlos Relanno <carlosvk97@gmail.com>
2021-10-10 12:51:26 -0400

    more changes

--- Assignment_1/classes.rb
+++ Assignment_1/classes.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class SeedStock
   attr_accessor :seed_stock
   attr_accessor :mutant_gene_id
@@ -6,16 +8,28 @@ class SeedStock
   attr_accessor :grams_remaining
   attr_accessor :gene
 
-
   def initialize(params={})
     @seed_stock = params.fetch(:seed_stock, "unknown")
     @mutant_gene_id = params.fetch(:mutant_gene_id, "unknown")
     @gene = params.fetch(:gene, "unknown")
     @last_planted = params.fetch(:last_planted, "unknown")
     @storage = params.fetch(:storage, "unknown")
-    @grams_remaining = params.fetch(:grams_remaining, "unknown")
-    
+    @grams_remaining = params.fetch(:grams_remaining, "unknown").to_i
   end
+  
+  def plant(n)
+    if n <= 0 # Just to avoid changing the date if planting 0 seeds
+      return
+    end
+    
+    if @grams_remaining > n
+      @grams_remaining -= n
+    else
+      puts "WARNING: we have run out of Seed Stock #{@seed_stock}"
+      @grams_remaining = 0
+    end
+    @last_planted = DateTime.now.strftime "%d/%m/%Y" # Set today as the last planted date
+  end   
 end
 
 
@@ -24,7 +38,6 @@ class Gene
   attr_accessor :gene_name
   attr_accessor :mutant_phenotype
 
-  
   def initialize(params = {})
     @gene_id = params.fetch(:gene_id, "unknown")
     @gene_name = params.fetch(:gene_name, "unknown")
@@ -43,7 +56,7 @@ class Database
     @genes = Hash.new() # This will store all gene objects
     gene_information = IO.readlines(@gene_info_file)
     for item in gene_information[1..-1]
-      item = item.split("\t")
+      item = item.chomp.split("\t")
       @genes[item[0].to_sym] = Gene.new(gene_id: item[0],
                                         gene_name: item[1],
                                         mutant_phenotype: item[2])
@@ -52,8 +65,9 @@ class Database
     @seed_stock_file = params.fetch(:seed_stock_file)
     @stock = Hash.new()
     seed_stock_info = IO.readlines(@seed_stock_file)
+    @seed_stock_header = seed_stock_info[0]
     for item in seed_stock_info[1..-1]
-      item = item.split("\t")
+      item = item.chomp.split("\t")
       @stock[item[0].to_sym] = SeedStock.new(seed_stock: item[0],
                                              mutant_gene_id: item[1],
                                              last_planted: item[2],
@@ -61,5 +75,14 @@ class Database
                                              grams_remaining: item[4],
                                              gene: @genes[item[1].to_sym])
     end
+  end
+  
+  def write_database
+    file = File.open(@seed_stock_file.gsub("seed_stock_data", "new_stock_file"), "w")
+    file.write(@seed_stock_header)
+    for item in @stock.values
+      file.write("#{item.seed_stock}\t#{item.mutant_gene_id}\t#{item.last_planted}\t#{item.storage}\t#{item.grams_remaining}\n")
+    end
+    file.close
   end
 end
--- Assignment_1/main.rb
+++ Assignment_1/main.rb
@@ -1,9 +1,12 @@
 require_relative './classes'
 
+# Database creation
 database = Database.new(gene_info_file: "/home/osboxes/Assignment-Answers/Assignment_1/StockDatabaseDataFiles/gene_information.tsv",
                         seed_stock_file: "/home/osboxes/Assignment-Answers/Assignment_1/StockDatabaseDataFiles/seed_stock_data.tsv",
                         cross_file: "/home/osboxes/Assignment-Answers/Assignment_1/StockDatabaseDataFiles/cross_data.tsv")
 
-
-puts database.stock.keys
-puts database.stock[:A334].inspect
+# Planting 7 seeds of each seed and updating into new_stock_file.tsv
+for key in database.stock.keys
+  database.stock[key].plant(7)
+end
+database.write_database
