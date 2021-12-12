e13963ff03107ffb5dfc03593adf5fb64f7f7006
Carlos Relanno <carlosvk97@gmail.com>
2021-12-12 17:51:12 -0500

    only details left

--- Assignment_4/main.rb
+++ Assignment_4/main.rb
@@ -1,7 +1,8 @@
 require 'bio'
 require 'stringio'
+require 'ruby-progressbar'
 
-# 1. Create the databases
+# Create the databases
 tair_path = 'Databases/TAIR10_cds_20101214_updated.fa' # DNA
 pep_path = 'Databases/pep.fa' # Protein
 
@@ -18,24 +19,30 @@ factory_tair = Bio::Blast.local('tblastn', 'Databases/TAIR', '-e 10e-6')
 tair_file = Bio::FlatFile.auto(tair_path) # 35386 entries
 pep_file = Bio::FlatFile.auto(pep_path) # 5146 entries
 
+output = File.new('orthologs.txt', 'w')
+output.puts "PEP_id\tTAIR_id\tPEP>TAIR_evalue\tTAIR>PEP_evalue"
 
+orthologs = Hash.new()
 # We will start with the pep entries, since there are less of them
 
+count = 0
 pep_file.each_entry() do |entry|
+  count +=1
+  break if count > 100
   report = factory_tair.query(entry) 
   
   next unless report.hits.any? # Skip if it does not find matches
   
-  puts entry.definition, entry.entry_id, "#{report.hits.length} hits found"
+  #puts entry.definition, entry.entry_id, "#{report.hits.length} hits found"
   first_hit = report.hits[0]
   #puts first_hit.evalue, first_hit.definition
   
   # Search the entry with the definition of the first hit
   hit = nil
-  tair_file.rewind()
+  tair_file.rewind() # Reset file pointer to the start of the flatfile
   tair_file.each_entry() do |entry2|
     if first_hit.definition.split('|')[0].rstrip == entry2.entry_id
-      puts 'Best hit found in plant database'
+      #puts 'Best hit found in plant database'
       hit = entry2
       break
     end
@@ -44,23 +51,26 @@ pep_file.each_entry() do |entry|
   if hit.nil?
     puts 'Could not find the best hit in the database'
     puts first_hit.definition, "\n"
-    next
+    exit()
   end
   
   # Query the best hit against the pep database
   report2 = factory_pep.query(hit)
-  puts "#{report2.hits.length} hits found"
+  #puts "#{report2.hits.length} hits found"
   best_second_hit = report2.hits[0]
-  puts best_second_hit.definition.split('|')[0]
+  #puts best_second_hit.definition.split('|')[0]
+  
+  
+  # Check if the hits are recyprocal
   if report2.hits[0].definition == entry.definition
-    puts 'OJOOOO', report2.hits[0].definition, entry.definition
+    puts 'Found one!'
+    orthologs[entry.entry_id] = hit.entry_id
+    output.puts "#{entry.entry_id}\t#{hit.entry_id}\t#{first_hit.evalue}\t#{best_second_hit.evalue}" 
   end
-  puts "\n"
+  #puts "\n"
 end
-#  print "#{hit.hit_id} : evalue #{hit.evalue}\t#{hit.target_id} at "
-#  puts "#{hit.lap_at}"   # this tells you start and end of both the query and the hit sequences
-#  hit.each do |hsp|
-#    puts hsp.qseq  # this is the gapped Alignment as text of the query
-#    puts hsp.hseq  # this is the gapped Alignment as text of the hit
-#  end
-#end
+
+puts orthologs
+output.close
+
+
